e3f8929d0dfa7a134bfc02316a31d42db10c8de6
Taylor Lohman <taylor.lohman.tl@gmail.com>
2020-12-11 18:19:01 -0800

    configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
 // MARK: -- override defaults here
-module.exports = () => ({
-	output: {
-		filename: "bundle.js"
-	},
-	plugins: [new HTMLWebpackPlugin(), new webpack.ProgressPlugin()]
-})
+module.exports = ({ mode, present } = { mode: "production", presets: [] }) => {
+	console.log(mode);
+	return {
+		mode,
+		output: {
+			filename: "bundle.js"
+		},
+		plugins: [new HTMLWebpackPlugin(), new webpack.ProgressPlugin()]
+	};
+};
